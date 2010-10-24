f12f7bbaec57fa1572eeded5e30167faa5325bca
Ravi Parikh <rparikh@dn0a2031d0.sunet>
2010-10-24 13:48:56 -0700

    upgraded some styling stuff

--- web/streets.js
+++ web/streets.js
@@ -28,11 +28,6 @@ map.add(po.geoJson()
     .attr("class", "tractClass")
     .attr("tractnum", function(d) { return d.properties.NAME; })
     .attr("id", function(d) { i++; return "id" + i; })
-    /*
-    .attr("onclick", function(d) {
-      return "$('#tooltip').html('Tract number: " + i + " " + d.properties.NAME + "')";
-    })
-    */
 	.style("fill", function(d) { return "green"; })
     .title(function(d) { return d.properties.NAME; })));
 
@@ -40,42 +35,7 @@ map.add(po.compass()
     .pan("none"));
 
 //add handlers
-
+setTimeout("$('.tractClass').click(function() { $('#tooltip').html('CLICKED!') })", 5000);
 setTimeout("$('.tractClass').mouseover(function() { $('#tooltip').html('tractnum: ' + $(this).attr('tractnum')); $(this).attr('stroke', 'white'); $(this).css('fill', 'white')});", 5000);
 setTimeout("$('.tractClass').mouseleave(function() { $(this).attr('stroke', 'black'); $(this).css('fill', 'green') });", 5000);
 setTimeout("$('#loading').hide()", 5500);
-
-
-/*
-$(document).ready(function() {
-  alert('awef');
-  $("#id2").click(function() {
-    $('#tooltip').html('clicked');
-  });
-});
-*/
-/*
-// Creates canvas 320 × 200 at 10, 50
-var paper = Raphael(10, 50, 320, 200);
-
-// Creates circle at x = 50, y = 40, with radius 10
-var circle = paper.circle(50, 40, 10);
-// Sets the fill attribute of the circle to red (#f00)
-circle.attr("fill", "#f00");
-
-// Sets the stroke attribute of the circle to white
-circle.attr("stroke", "#fff");
-*/
-
-
-
-/*
-function load(e) {
-  for (var i = 0; i < e.features.length; i++) {
-    var feature = e.features[i];
-    feature.element.appendChild(po.svg("title").appendChild(
-        document.createTextNode("Ravi"))
-        .parentNode);
-  }
-}
-*/
