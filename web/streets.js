2638831cef2efa70ef394f1435ea68127c17316c
Ravi Parikh <rparikh@dn0a2031d0.sunet>
2010-10-24 13:23:35 -0700

    added a loading gif, got some hover effects to work

--- web/streets.js
+++ web/streets.js
@@ -6,8 +6,8 @@ var color = pv.Scale.linear()
 
 var map = po.map()
     .container(document.getElementById("map").appendChild(po.svg("svg")))
-    .center({lat: 37.76, lon: -122.44})
-    .zoom(13)
+    .center({lat: 64, lon: -142.44})
+    .zoom(4)
     .zoomRange([3, 16])
     .add(po.interact());
 
@@ -17,15 +17,65 @@ map.add(po.image()
     + "/999/256/{Z}/{X}/{Y}.png")
     .hosts(["a.", "b.", "c.", ""])));
 
+var i = 0;
 map.add(po.geoJson()
     .url("alaska.json")
     .id("streets")
     .zoom(5)
     .tile(false)
   .on("load", po.stylist()
-    .attr("stroke", function(d) { return "black"; })
-	.style("fill", function(d) { if(d.properties.NAME == "0002.02" || d.properties.NAME == "0001") {return "red";} else return "green"; })
+    .attr("stroke", "black")
+    .attr("class", "tractClass")
+    .attr("tractnum", function(d) { return d.properties.NAME; })
+    .attr("id", function(d) { i++; return "id" + i; })
+    /*
+    .attr("onclick", function(d) {
+      return "$('#tooltip').html('Tract number: " + i + " " + d.properties.NAME + "')";
+    })
+    */
+	.style("fill", function(d) { return "green"; })
     .title(function(d) { return d.properties.NAME; })));
 
 map.add(po.compass()
     .pan("none"));
+
+//add handlers
+
+setTimeout("$('.tractClass').mouseover(function() { $('#tooltip').html('tractnum: ' + $(this).attr('tractnum')); $(this).attr('stroke', 'white'); $(this).css('fill', 'white')});", 5000);
+setTimeout("$('.tractClass').mouseleave(function() { $(this).attr('stroke', 'black'); $(this).css('fill', 'green') });", 5000);
+setTimeout("$('#loading').hide()", 5500);
+
+
+/*
+$(document).ready(function() {
+  alert('awef');
+  $("#id2").click(function() {
+    $('#tooltip').html('clicked');
+  });
+});
+*/
+/*
+// Creates canvas 320 × 200 at 10, 50
+var paper = Raphael(10, 50, 320, 200);
+
+// Creates circle at x = 50, y = 40, with radius 10
+var circle = paper.circle(50, 40, 10);
+// Sets the fill attribute of the circle to red (#f00)
+circle.attr("fill", "#f00");
+
+// Sets the stroke attribute of the circle to white
+circle.attr("stroke", "#fff");
+*/
+
+
+
+/*
+function load(e) {
+  for (var i = 0; i < e.features.length; i++) {
+    var feature = e.features[i];
+    feature.element.appendChild(po.svg("title").appendChild(
+        document.createTextNode("Ravi"))
+        .parentNode);
+  }
+}
+*/
