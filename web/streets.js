f779d7921c82791bbae0e264ba259446c16edbf5
Ravi Parikh <rparikh@dn51u3so.sunet>
2010-10-27 02:03:58 -0700

    added state names dictionary to file, tooltip modified

--- web/streets.js
+++ web/streets.js
@@ -1,3 +1,5 @@
+var state_dict = {'01':'Alabama', '02':'Alaska', '04':'Arizona', '05':'Arkansas', '06':'California', '08':'Colorado', '09':'Connecticut', '10':'Delaware', '12':'Florida', '13':'Georgia', '15':'Hawaii', '16':'Idaho', '17':'Illinois', '18':'Indiana', '19':'Iowa', '20':'Kansas', '21':'Kentucky', '22':'Louisiana', '23':'Maine', '24':'Maryland', '25':'Massachusetts', '26':'Michigan', '27':'Minnesota', '28':'Mississippi', '29':'Missouri', '30':'Montana', '31':'Nebraska', '32':'Nevada', '33':'New Hampshire', '34':'New Jersey', '35':'New Mexico', '36':'New York', '37':'North Carolina', '38':'North Dakota', '39':'Ohio', '40':'Oklahoma', '41':'Oregon', '42':'Pennsylvania', '44':'Rhode Island', '45':'South Carolina', '46':'South Dakota', '47':'Tennessee', '48':'Texas', '49':'Utah', '50':'Vermont', '51':'Virginia', '53':'Washington', '54':'West Virginia', '55':'Wisconsin', '56':'Wyoming', '72':'Puerto Rico'};
+
 String.prototype.capitalize = function(){
    return this.replace( /(^|\s)([a-z])/g , function(m,p1,p2){ return p1+p2.toUpperCase(); } );
   };
@@ -310,8 +312,8 @@ $(function(){
 					}
 					else {
 					  $('.stateClass').click(function() {
+					    
 						var id = $(this).attr('stateid').substr(-2);
-
 						$.ajax({
 						        url: 'http://204.232.210.102:5011/query/lar',
 						        data: {fields:"state,income", state:id},
@@ -340,7 +342,10 @@ $(function(){
 						});
 					  })
 					  .mouseover(function(e) {
-						$('.tipBody').text(metricType + ': ' + $(this).attr('metric'));
+					    var userFriendlyMetric = 'Rate Spread';
+					    if(metricType == 'income') { userFriendlyMetric = 'Income'; }
+					    if(metricType == 'loan_amount') { userFriendlyMetric = 'Loan Amount'; }
+						$('.tipBody').text(state_dict[$(this).attr('stateid').substr(-2)] + ' - ' + userFriendlyMetric + ': ' + $(this).attr('metric'));
 				        //Set the X and Y axis of the tooltip  
 				        $('#tooltip').css('top', e.pageY + 10 );  
 				        $('#tooltip').css('left', e.pageX + 20 );  
